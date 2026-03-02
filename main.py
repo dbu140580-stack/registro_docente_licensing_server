151f2e6a8c663cc96ac95233a4b2d0063dde143c
Diego B <hdiegoburini@gmail.com>
2026-03-02 19:47:14 -0300

    Make admin header mandatory

--- main.py
+++ main.py
@@ -18,7 +18,10 @@ app = FastAPI()
 ADMIN_API_KEY = os.getenv("ADMIN_API_KEY")
 
 
-def verify_admin_key(x_api_key: str = Header(None)):
+def verify_admin_key(x_api_key: str = Header(...)):
+    if ADMIN_API_KEY is None:
+        raise HTTPException(status_code=500, detail="ADMIN_API_KEY no configurada")
+
     if x_api_key != ADMIN_API_KEY:
         raise HTTPException(status_code=403, detail="No autorizado")
 
